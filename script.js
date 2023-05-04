01f1b032be8c1c4916894a48172735da7a15a9ed
Z964B00 <z.964b00@gmail.com>
2023-05-04 08:28:58 -0400

    Create message

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const messageComponents = {
     wordThree: ['JavaScript', 'woodworking', 'instructional design', 'Italian'],
     wordFour: ['focus on errors', 'never give up', 'try and try again', 'ask for help']
 }
+
+const selectWords = (obj) => {
+    const selectedWords = [];
+    selectedWords.push(messageComponents.wordOne[Math.floor(Math.random() * 4)]);
+    selectedWords.push(messageComponents.wordTwo[Math.floor(Math.random() * 4)]);
+    selectedWords.push(messageComponents.wordThree[Math.floor(Math.random() * 4)]);
+    selectedWords.push(messageComponents.wordFour[Math.floor(Math.random() * 4)]);
+    console.log(`The ${selectedWords[0]} way to ${selectedWords[1]} ${selectedWords[2]} is to ${selectedWords[3]}.`);
+}
+
+selectWords(messageComponents);
